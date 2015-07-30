bf8f6aa4eb3ea8a1c748e834dc6a8613ece3605b
Daniele Moraschi <daniele.moraschi@dxi-easycall.net>
2015-07-30 19:09:59 +0100

    upd tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 var HttpResponses = require('../lib/index');
-var Middleware = require('../lib/index').HttpResponseMiddleware;
+var Middleware = require('../lib/index').Middleware;
 var util = require('util');
 var assert = require('assert');
 
@@ -110,4 +110,42 @@ describe('Http Responses', function() {
         });
 
     });
+
+    describe('HttpResponseMiddleware', function() {
+
+        it('should match the expected result', function() {
+            var name, req, res, next, response;
+            for (var i = 0; i < responses.length; i++) {
+
+                name = responses[i];
+                req = {};
+                res = {
+                    status: function (status) {
+                        this.status = status;
+                        var self = this;
+                        return {
+                            send: function (response) {
+                                self.response = response;
+                            }
+                        }
+                    }
+                };
+                next = function () {};
+
+                console.info('checking ' + name);
+                response = new HttpResponses[name]();
+
+                Middleware(response, req, res, next);
+
+                assert.equal(res.response, response);
+
+                if (statuses[name].status) {
+                    assert.equal(res.status, response.status);
+                    assert.equal(res.status, statuses[name].status);
+                }
+            }
+        });
+
+    });
+
 });
